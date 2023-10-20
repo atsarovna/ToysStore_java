67193224318574e5fa5d2d14c61ca5032b13fca4
Evgenia Mun <evgenya.mun@yandex.ru>
2023-10-21 02:29:07 +0300

    add new names and updateToyWeight

--- ToyStore.java
+++ ToyStore.java
@@ -2,11 +2,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ToyStore {
-    private List<Toy> toys = new ArrayList<>();
+    private List<Toy> availableToys = new ArrayList<>();
 
     //добавление новой игрушки в магазин
-    public void addToy(int toyID, String name, int quantity, double weight) {
-        Toy toy = new Toy(toyID, name, quantity, weight);
-        toys.add(toy);
+    public void addNewToy(int toyId, String toyName, int toyQuantity, double toyWeight) {
+        Toy newToy = new Toy(toyId, toyName, toyQuantity, toyWeight);
+        availableToys.add(newToy);
+    }
+
+    // Обновление веса (частоты выпадения) игрушки
+    public void updateToyWeight(int toyId, double newToyWeight) {
+        for (Toy toy : availableToys) {
+            if (toy.getToyId() == toyId) {
+                toy.setToyWeight(newToyWeight);
+                break;
+            }
+        }
     }
 }
